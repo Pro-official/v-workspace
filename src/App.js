c00043cf75c23d9472a8bd78eec46899bb371a6b
Pro-official <promise.iam00@gmail.com>
2022-08-22 23:33:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Registration/Register.jsx";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ src/pages/Shared/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfsPBk3oM_Uu0rcq496ctwMQOZFKxM3Jw",
+  authDomain: "v-classspace.firebaseapp.com",
+  projectId: "v-classspace",
+  storageBucket: "v-classspace.appspot.com",
+  messagingSenderId: "431989640582",
+  appId: "1:431989640582:web:cb58dfcad524cfa4457007",
+  measurementId: "G-H6YP8G48R1",
+};
+
+export default firebaseConfig;
